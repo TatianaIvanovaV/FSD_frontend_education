1b65bad48c3a9e5841bee1cad9b8af77b4319580
Tatiana Ivanova <komarivanova@gmail.com>
2020-10-10 01:02:50 +0300

    complete rage-price block+js

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,29 @@
 'use strict';
 
 import $ from 'jquery'
+import 'slick-carousel'
+import noUiSlider from 'nouislider'
+/*-- range-slider --*/
+var unconstrainedSlider = document.getElementById('unconstrained');
+var unconstrainedValues = document.getElementById('unconstrained-values');
+noUiSlider.create(unconstrainedSlider, {
+  start: [5000, 10000],
+  behaviour: 'unconstrained-tap',
+  connect: true,
+  range: {
+      'min': 0,
+      'max': 15000,
+  },
+  format: {
+    from: function(value) {
+          return parseInt(value);
+        },
+    to: function(value) {
+          return parseInt(value)+ "₽";
+        }
+    },
+    
+});
+unconstrainedSlider.noUiSlider.on('update', function (values) {;
+  unconstrainedValues.innerHTML = values.join(' - ');
+});
