ec0e001cb76ce1cddf36c7100a85f9a85b79b285
Tatiana Ivanova <komarivanova@gmail.com>
2020-12-06 00:01:41 +0300

    header nav-links arrows rotate

--- src/js/nav-link.js
+++ src/js/nav-link.js
@@ -1,6 +1,9 @@
 $('.header__link').click(function(e){ 
-  e.preventDefault();                
+  
+  e.preventDefault(); 
+  $(this).children('.header__icon--down').toggleClass('header__icon--rotate');               
   var item = $(this).closest('li'); 
   var sub = item.find('.header__sub-list');
   sub.slideToggle();
+  
  });
