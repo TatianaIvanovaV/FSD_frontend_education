7f07782a4230574c50c19126e015b05def33358a
Tatiana Ivanova <komarivanova@gmail.com>
2020-12-23 02:11:18 +0300

    remove hamburger-menu mixin

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import "./modules/room-card/carousel.js"
 import "./modules/reviews/like.js"
 import "./modules/dropdown-select-counters/dropdown-select-counters.js"
-import "./modules/hamburger-menu/hamburger-menu.js"
+import "./modules/header/header.js"
 import "./modules/range-slider/range-slider.js"
 import "./modules/filter-button/filter-button.js"
 import "./modules/date-dropdown/date-dropdown.js"
